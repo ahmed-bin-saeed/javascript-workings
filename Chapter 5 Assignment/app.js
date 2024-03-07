4d35c3257f24aacf9ff83abf8b8346255f2aaad3
Ahmed bin Saeed <160342461+ahmed-bin-saeed@users.noreply.github.com>
2024-03-07 17:10:50 +0500

    Add files via upload

---
+++ Chapter 5 Assignment/app.js
@@ -0,0 +1,37 @@
+// let firstNum = 10;
+// let secNum = 5;
+// let addition = firstNum + secNum;
+// document.write(" Sum Of ", firstNum, " and ", secNum, " is ", addition, "<br />");
+// console.log(" Sum Of ", firstNum, " and ", secNum, " is ", addition);
+// let subtraction = firstNum - secNum;
+// document.write(" Subtraction Of ", firstNum, " and ", secNum, " is ", subtraction, "<br />");
+// console.log(" Subtraction Of ", firstNum, " and ", secNum, " is ", subtraction);
+
+// let multiplication = firstNum * secNum ;
+// console.log(" Multiplication Of ", firstNum, " and ", secNum, " is ", multiplication);
+// document.write(" Multiplication Of ", firstNum, " and ", secNum, " is ", multiplication, "<br />");
+
+// let division = firstNum / secNum;
+// console.log(" Division Of ", firstNum, " and ", secNum, " is ", division);
+// document.write(" Division Of ", firstNum, " and ", secNum, " is ", division, "<br />");
+
+// let modulus = firstNum % secNum;
+// console.log(" Modulus Of ", firstNum, " and ", secNum, " is ", modulus);
+// document.write("Modulus Of ", firstNum, " and ", secNum, " is ", modulus);
+
+let myValue;
+document.write("Value After Variable Declaration Is ", myValue, "<br />");
+myValue = 5;
+document.write("Initial Value ", myValue, "<br />");
+
+myAnotherValue = ++ myValue;
+document.write("Value after increment is ", myValue, "<br />");
+
+myValue = myValue + 7;
+document.write("Value after Addition is ", myAnotherValue, "<br />")
+
+myAnotherValue = --myValue;
+document.write("Value After Decrement Is ", myValue, "<br />");
+
+myValue = myValue % 3;
+document.write("The Remainer is ", myValue);
