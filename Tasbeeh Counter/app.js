16a61ee6382624f4629db20457416e5c5a52ce70
Ahmed bin Saeed <160342461+ahmed-bin-saeed@users.noreply.github.com>
2024-03-23 19:32:17 +0500

    Add files via upload

---
+++ Tasbeeh Counter/app.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+document.getElementById('add').onclick = function () {
+    count++;
+    document.getElementById('zero').innerHTML = count;
+}
+
+document.getElementById('reset').onclick = function () {
+    count = 0;
+    document.getElementById('zero').innerHTML = count;
+}
